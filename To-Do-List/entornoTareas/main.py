51ef2a5943effcf7ade9c3c9329b913fa99fd3e1
estela medina <122041816@upq.edu.mx>
2025-02-14 15:30:12 -0600

    endpoint para actualizar una tarea

--- To-Do-List/entornoTareas/main.py
+++ To-Do-List/entornoTareas/main.py
@@ -19,14 +19,17 @@ tareas = [
     {"id": 7, "titulo": "Aprender FastAPI", "descripcion": "Ver tutoriales y practicar", "vencimiento": "2024-03-10", "estado": "no completada", "profesor": "Cecilia"}
 ]
 
+#endpoint de inicio 
 @app.get("/", tags=['Inicio'])
 def main():
     return {"message": "¡Bienvenido a la API de Gestión de Tareas!"}
 
+#endpoint obtener todas las tareas
 @app.get("/tareas", tags=['Operaciones CRUD'])
 def obtener_todas_las_tareas():
     return {'Tareas Registradas': tareas}
 
+#enpointe obtener una tarea por ID
 @app.get("/tareas/{id}", tags=['Operaciones CRUD'])
 def obtener_tarea(id: int):
     for tarea in tareas:
@@ -34,6 +37,7 @@ def obtener_tarea(id: int):
             return tarea
     raise HTTPException(status_code=404, detail="Tarea no encontrada")
 
+#endpoint crear una nueva tarea
 @app.post("/tareas", tags=['Operaciones CRUD'])
 def crear_tarea(tarea: dict):
     for t in tareas:
@@ -41,3 +45,12 @@ def crear_tarea(tarea: dict):
             raise HTTPException(status_code=400, detail="La tarea ya existe")
     tareas.append(tarea)
     return tarea
+
+#endpoint actualizar
+@app.put("/tareas/{id}", tags=['Operaciones CRUD'])
+def actualizar_tarea(id: int, tarea_actualizada: dict):
+    for index, tarea in enumerate(tareas):
+        if tarea["id"] == id:
+            tareas[index].update(tarea_actualizada)
+            return tareas[index]
+    raise HTTPException(status_code=404, detail="Tarea no encontrada")
